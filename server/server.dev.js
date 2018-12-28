6f89609a08127394cbff96adcd31f95274cd035e
venepe <venepe@users.noreply.github.com>
2018-12-27 21:18:37 -0600

    Add logger concept

--- server/server.dev.js
+++ server/server.dev.js
@@ -6,6 +6,11 @@ import invariant from 'invariant';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackDevConfig from '../webpack/webpack.dev.babel';
+import {
+  logAudioSession,
+  logInteractiveSession,
+  logSearchText,
+} from './logger';
 const { Pool } = require('pg');
 const { postgraphile } = require('postgraphile');
 import {
@@ -28,8 +33,26 @@ app.use(bodyParser.json());
 
 app.use('/graphql', (req, res, next) => {
   if (req.body) {
+    const { operationName, variables } = req.body;
+    if (operationName === 'searchDecks') {
+      logSearchText({ pool, text: variables.search });
+    }
   }
   next();
+});
+
+app.post('/interactive-sessions',(req, res) => {
+  const { interactiveSession } = req.body || {};
+  const { uid, userId = null, audiocardId, deckId, response, isCorrect } = interactiveSession;
+  logInteractiveSession({ pool, uid, userId, audiocardId, deckId, response, isCorrect });
+  res.json({status: 'ok'})
+});
+
+app.post('/audio-sessions',(req, res) => {
+  const { audioSession } = req.body;
+  const { uid, userId = null, audiocardId, deckId } = audioSession;
+  logAudioSession({ pool, uid, userId, audiocardId, deckId });
+  res.json({status: 'ok'})
 });
 
 //GraphQL Api
